a968a4aefbb133597714024362833e76c99752c0
Akarshan123 <akarshanrehan@gmail.com>
2016-09-26 23:50:55 +0530

    Add files via upload

---
+++ renaming.bat
@@ -0,0 +1,5 @@
+@echo off
+move C:\Users\%username%\Desktop\MP3\3.mp3 C:\Users\%username%\Desktop\MP3\3.xls
+pause
+move C:\Users\%username%\Desktop\MP3\4.mp3 C:\Users\%username%\Desktop\MP3\4.xls
+pause
