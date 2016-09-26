ed1b18f3d9b860953ea68de16b9494238b28e581
Akarshan123 <akarshanrehan@gmail.com>
2016-09-27 00:37:59 +0530

    Delete renaming.bat

--- renaming.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-move C:\Users\%username%\Desktop\MP3\3.mp3 C:\Users\%username%\Desktop\MP3\3.xls
-pause
-move C:\Users\%username%\Desktop\MP3\4.mp3 C:\Users\%username%\Desktop\MP3\4.xls
-pause
