3f598d3630f2ace7f1bb3b4f54bee984b531e7e6
Akarshan123 <akarshanrehan@gmail.com>
2016-09-28 22:23:47 +0530

    Add files via upload

---
+++ MP3/renaming.bat
@@ -0,0 +1,8 @@
+@echo off
+color 0a
+move C:\Users\%username%\Desktop\MP3\3.mp3 C:\Users\%username%\Desktop\MP3\3.xls
+pause 
+move C:\Users\%username%\Desktop\MP3\4.mp3 C:\Users\%username%\Desktop\MP3\4.xls
+pause
+:: Author's Name - Akarshan Rehan
+:: Creation Time - 28/09/2016 10.00 P.M
